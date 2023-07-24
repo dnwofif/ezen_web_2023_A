4e5d45b0c9712f2acdd38ab371543a623332a340
504 <504@DESKTOP-TTN284H>
2023-07-24 10:23:54 +0900

    day33_복습part1

---
+++ backend/src/복습/part1/part1.java
@@ -0,0 +1,22 @@
+package 복습.part1;
+
+import java.util.Scanner;
+
+public class part1 {//class s
+	public static void main(String[] args) {//main s
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int 콜라재고=10;		int 환타재고=3;		int 사이다재고=2;
+		int 콜라바구니=0;		int 한타바구니=0;		int 사이다바구니=0;
+		int 콜라가격=300;		int 환타가격=200;		int 사이다가격=100;
+		
+		while(true) {//while s
+			System.out.println("------------------메뉴------------------");
+			System.out.println("1.콜라[300] 2.환타[200] 3.사이다[100] 4.결제");
+			System.out.println(">>>>>> 선택 : ");
+			
+		
+		}//while e
+	}//main e
+}//class e
