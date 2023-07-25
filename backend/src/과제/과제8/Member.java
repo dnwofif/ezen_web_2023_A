ea35ba663585b1d1392fa2df5dbeb0a943adfb51
504 <504@DESKTOP-TTN284H>
2023-07-25 12:16:11 +0900

    day35 회원제시스템

---
+++ backend/src/과제/과제8/Member.java
@@ -0,0 +1,15 @@
+package 과제.과제8;
+
+//목적 : 설계도,객체의모델링 표본
+public class Member {
+	//1.필드 : 객체의 데이터를 저장하는 곳
+	String id;	//초기값/처음에 대입이 없을때 기본값[정수0실수0.0불 false 참조 null]
+	String password;
+	String name;
+	String phone;
+	int age;
+	//2.생성자
+	
+	//3.메소드
+
+}
