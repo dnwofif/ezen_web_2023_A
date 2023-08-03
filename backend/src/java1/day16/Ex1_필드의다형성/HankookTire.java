4ba06055b4bed354d84a70d137cc7bde90271f39
504 <504@DESKTOP-OM2NF7O>
2023-08-03 14:13:24 +0900

    day41 필드의다형성

---
+++ backend/src/java1/day16/Ex1_필드의다형성/HankookTire.java
@@ -0,0 +1,13 @@
+package java1.day16.Ex1_필드의다형성;
+
+public class HankookTire {
+	//상속 : 자식클래스 extends 부모클래스
+
+//오버라이딩 : 부모클래스의 메소드를 불려받았지만 재정의
+	public void roll() {
+		// TODO Auto-generated constructor stub
+		System.out.println("한국 Tire 회전");
+		
+	}
+	
+}
