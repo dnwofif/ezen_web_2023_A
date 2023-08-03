2379fee18b98d902026fe5a28fe69b0788360ebe
504 <504@DESKTOP-OM2NF7O>
2023-08-03 19:49:43 +0900

    part5

---
+++ backend/src/복습/part5/Member.java
@@ -0,0 +1,27 @@
+package 복습.part5;
+
+import java.util.Objects;
+
+public class Member {
+	String id;
+	String password;
+	String name;
+	String phone;
+	int age;
+	
+	public Member(String id, String password, String name, String phone, int age) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.phone = phone;
+		this.age = age;
+	}
+	
+	
+	
+
+	
+	
+	
+}
