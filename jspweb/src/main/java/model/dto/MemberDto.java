70cff2444bdf060d7338e606b7342bf40643ecad
504 <504@DESKTOP-OM2NF7O>
2023-08-31 11:26:06 +0900

    day60 로그인성공시 loginDto를 세션에 저장

--- jspweb/src/main/java/model/dto/MemberDto.java
+++ jspweb/src/main/java/model/dto/MemberDto.java
@@ -3,11 +3,25 @@ package model.dto;
 public class MemberDto {
 	
 	//1. 필드
+		//1-1. DB 있는 필드
 	private int mno;
 	private String mid;
 	private String mpwd;
 	private String memail;
 	private String mimg;
+	
+		//1-2. DB 없는 필드
+	private String loginDatetime;	// 로그인 시간
+	
+	// -- * 로그인 객체를 만들 생성자 [ 패스워드 제외 ] : 오버로딩 하기위해 매게변수 타입 순서 변경
+	public MemberDto(String loginDatetime, int mno, String mid, String memail, String mimg) {
+		super();
+		this.mno = mno;
+		this.mid = mid;
+		this.memail = memail;
+		this.mimg = mimg;
+		this.loginDatetime = loginDatetime;
+	}
 	
 	//2.생성자
 	public MemberDto() {}
@@ -75,8 +89,9 @@ public class MemberDto {
 	@Override
 	public String toString() {
 		return "MemberDto [mno=" + mno + ", mid=" + mid + ", mpwd=" + mpwd + ", memail=" + memail + ", mimg=" + mimg
-				+ "]";
+				+ ", loginDatetime=" + loginDatetime + "]";
 	}
+
 	
 	
 	
