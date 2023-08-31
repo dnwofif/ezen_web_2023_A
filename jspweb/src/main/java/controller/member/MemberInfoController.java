9674fe3957e89cd38b46573da31fa26708faf2b8
504 <504@DESKTOP-OM2NF7O>
2023-08-31 12:47:59 +0900

    day60 로그인상태에 따른 서브메뉴 출력 , 로그아웃 구현

--- jspweb/src/main/java/controller/member/MemberInfoController.java
+++ jspweb/src/main/java/controller/member/MemberInfoController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
@@ -32,14 +33,17 @@ public class MemberInfoController extends HttpServlet {
    		
    	// ----------------------------------------------- 파일 업로드 : 서버폴더에 --------------------------------------------- //	
    		// 첨부파일 저장할 폴더의 절대경로
-   		//1. 개발자 pc 경로 업로드 [ 문제발생 : 개발자pc에 업로드하면 업로드파일을 서버로 빌드 ]
-   		//String uploadpath ="C:\\\\Users\\\\504\\\\git\\\\ezen_web_2023_A\\\\jspweb\\\\src\\\\main\\\\webapp\\\\member\\\\img"; // 첨부파일 저장할 경로
-   		//2. 서버 pc 경로 업로드 [사용자는 바로 서버pc 업로드, git에 못올림]
-   		
-   		//3. 서버 pc 경로 (상대경로 = 서버경로 찾아주는 함수)
-   			// 서버에 build(배포)된 파일/폴더의 경로 찾기
-   		String uploadpath = request.getSession().getServletContext().getRealPath("/member/img");
-   		System.out.println("member 폴더 img 폴더 실제(서버)경로 : "+uploadpath);
+   		// 1. 개발자 pc 경로 업로드 [ 문제발생 : 개발자pc에 업로드하면 업로드파일을 서버로 빌드 ]
+   		//String uploadpath ="C:\\Users\\504-t\\git\\ezen_web_2023_A\\jspweb\\src\\main\\webapp\\member\\img"; // 첨부파일 저장할 경로
+   		// 2. 서버 pc 경로 업로드  [ 사용자는 바로 서버pc 업로드 ]
+   		//String uploadpath ="C:\\Users\\504-t\\eclipse-workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\jspweb\\member\\img";
+   		// 3. 서버 pc 경로 ( 상대경로 = 서버경로 찾아주는 함수 )
+   			// 서버에 build(배포)된 파일/폴더 의 경로 찾기
+   			// request.getServletContext().getRealPath("프로젝트명이하 경로");
+   		String uploadpath = request.getServletContext().getRealPath("/member/img");
+   			// RealPath : C:\\Users\\504-t\\eclipse-workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\jspweb\\member\\img
+   			// HttpPath : http://localhost/jspweb/member/img/
+   		System.out.println( "member 폴더 img 폴더 실제(서버) 경로 : " + uploadpath ); 
    		// 첨부파일 전송 했을때.
    			// 1. 첨부파일 서버PC에 업로드( COS.jar 라이브러리 ) 
    				// MultipartRequest : 파일 업로드 클래스 
@@ -62,7 +66,7 @@ public class MemberInfoController extends HttpServlet {
    		String mimg =  multi.getFilesystemName("mimg");		System.out.println("mimg : "  + mimg);
    		
    		// *만약에 사진업로드 안했으면 기본프로필 사용하도록 변경 = default.webp
-   		if(mimg == null) {mimg = "default.webp";}
+   		if( mimg == null ) {mimg = "default.webp";}
    		
    		// 2. (선택) 객체화.
    		MemberDto memberDto = new MemberDto(mid, mpwd, memail, mimg);
@@ -99,26 +103,42 @@ public class MemberInfoController extends HttpServlet {
 	}
 	*/
 	
-	// 2. 
+	// 2. 회원정보(세션호출) / 로그아웃(세션초기화) 호출 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 1. 요청한다. [ 기능 구분을 위한 요청 ]
+		String type = request.getParameter("type");
+			// * 만약에 type 이 info이면 
+			if( type.equals("info") ){
+				// * 세션에 저장된 로그인객체를 꺼내기 
+				//1. 세션호출한다. [ 세션타입은 Object ]
+				Object session = request.getSession().getAttribute("loginDto");
+						//2. 타입변환한다. [ 부 -> 자  (캐스팅/강제타입변환) ]
+				MemberDto loginDto = (MemberDto)session;
+				
+					// * DTO는 JS가 이해할수 없는 언어 이므로 JS가 이해할수 있게 JS 언어로 변환 [jackson 라이브러리 ]
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json =  objectMapper.writeValueAsString( loginDto );
+				// 4. 응답한다.
+				response.setContentType("application/json; charset=utf-8");
+				response.getWriter().print(  json );
+			
+			}else if( type.equals("logout") ) {
+				// * 세션에 저장된 로그인객체를 없애기/초기화/지우기/삭제 
+				// 방법1 : (세션의 모든 속성) 초기화하는 함수 
+					// request.getSession().invalidate(); // 로그인 정보 뿐만 아니라 모두 삭제..
+				// 방법2 : (세션의 특정 속성) 초기화하는 방법 JVM GC(쓰레기수집기= 해당 객체를 아무도 참조하고 있으지 않으면 삭제 ) 
+					// 삭제할 세션속성명과 동일하게 null 대입
+				request.getSession().setAttribute("loginDto", null);
+			}
+	
 	}
-
-
-
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
+	// 3. 회원수정 
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
-
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
+	// 4. 회원삭제 
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
