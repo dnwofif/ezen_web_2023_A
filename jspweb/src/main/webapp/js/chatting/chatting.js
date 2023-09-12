5627e138438b5e69277bb51cd8055c1cda435a21
504 <504@DESKTOP-FDBPKQC>
2023-09-12 20:34:19 +0900

    day68 채팅구현하기 9.문제점보완[ 치환 , content 줄바꿈]

--- jspweb/src/main/webapp/js/chatting/chatting.js
+++ jspweb/src/main/webapp/js/chatting/chatting.js
@@ -1,3 +1,4 @@
+
 // * ajax(기본값 비동기통신) 가 응답의 결과를 가지고 오기전에 아래코드 실행. [ loginState 변경되기전에 실행되므로 문제발생 ]
 // - 해결방안 ajax 동기화 통신 하자.. 
 // 1. 비 로그인시 입장 불가능.. 
@@ -11,15 +12,18 @@ let clientSocket = new WebSocket(`ws://192.168.17.123:80/jspweb/serversokcet/${l
 	// - 서버소켓URL에 매개변수 전달하기 [- 주로 식별자 전달 ] 서버소켓URL/데이터1/데이터2/데이터3
 	// --- 메소드 4가지 메소드 자동으로 실행 
 		// 1. (자동실행) 클라이언트소켓이 정상적으로 서버소켓 접속했을때
-	clientSocket.onopen = e => { console.log('서버와 접속이 성공');
-			// 1-2 : 만약에 접속을 성공하면 알림메시지 전송
-			let msg = {
-				type : "alarm",
-				content : `${loginMid}님이 입장했습니다.`}
-			//clientSocket.send(msg);	
-			// 문제발생 : 해당 메시지를 받는 JAVA는 JSON타입 몰라 그래서 문자열 타입으로 전송
-			clientSocket.send(JSON.stringify(msg));
-	 	} ;
+	clientSocket.onopen = e => { 
+			
+		// 1-2 : 만약에 접속을 성공하면 알림메시지 전송 
+		let msg = { 
+			type : 'alarm' , 
+			content : `${loginMid}님이 입장했습니다.` 
+		}
+		// clientSocket.send( msg ); // 문제발생 : 해당 메시지를 받는 JAVA는 JSON타입 몰라요.. 그래서 문자열 타입으로 전송.
+		clientSocket.send( JSON.stringify( msg ) );
+			
+			
+	};
 		// 2. (자동실행) 클라이언트소켓이 서버소켓과 연결에서 오류가 발생했을때.
 	clientSocket.onerror = e => { console.log('서버와 오류발생:'+e ); };
 		// 3. (자동실행) 클라이언트소켓이 서버소켓과 연결이 끊겼을때.
@@ -48,37 +52,24 @@ function onSend(){
 
 // 4. 메시지를 받았을때 후추 행동(메소드) 선언 
 function onMsg( e ){
-	console.log( e ); // e : 메시지 받았을때 발생한 이벤트 정보가 들어있는 객체
-	console.log( e.data ); // .data 속성에 전달받은 메시지 내용 
-	// 1. 전달받은 내용물을 JSON타입으로 형변환
-	let msgBox = JSON.parse( e.data );	console.log( msgBox );
-	msgBox.msg = JSON.parse( msgBox.msg);
-	// 2. MSG속성내 content \n -> <br> 치환후 결과를 content속성에 대입
-		// - replace
-	msgBox.msg.content.replace(/\n/g,'<br>');
-		// JSON.parse( ) 		: 문자열타입의 JSON형식을 JSON타입으로 변환 
-		// JSON.stringify( ) 	: JSON타입 을 문자열 타입 (JSON형식 유지)으로 변환 
-		console.log( msgBox.msg ); // java,js console내 출력시 줄바꿈 \n 맞음.. html에서의 줄바꿈 <br>
-		
-		// 1. 특정 문자열 찾아서 1개 치환/바꾸기/교체 
-		let content = msgBox.msg.replace( '\n' , '<br>' );	// replace( '변경할문자열|정규표현식' , '새로운문자' );
-		console.log( content );
-		// 2. 특정 문자열 찾아서 찾은 문자열 모두 치환/바꾸기/교체 => java : .replaceAll();   js : 정규표현식 
-		content  = msgBox.msg.replace( /\n/g , '<br>');	// /g : 동일한 패턴의 모든 문자찾기[전체]
-		console.log( content );
-		
-	msgBox.msg	= JSON.parse( content ); // 치환하고 대입.
-		console.log( msgBox );
-		
+	
+	// 1. 전달받은 내용물을 JSON타입으로 형변환 
+	let msgBox = JSON.parse( e.data );		//  e.data : 서버로 부터 전달받은 내용물 e.data 속성에 있는 상태 
+	msgBox.msg = JSON.parse( msgBox.msg );
+	
+	// 2. msg속성내 content 속성의  \n -> <br> 치환후 결과를 content속성에 대입
+		// - replace( ) 문자열 교체/치환/바꾸기 함수 
+	msgBox.msg.content = msgBox.msg.content.replace( /\n/g , '<br>' );
+	
 	// 1. 어디에 출력할껀지 
 	let chatcont = document.querySelector('.chatcont')
 	// 2. 무엇을 
 	let html = ``;
-		// 만약에 알림 메시지이면
-		if( msgBox.msg.type == 'alarm'){
-			html = `${typeHTML(msgBox.msg)}`;
+		// 만약에 메시지 타입이 알림 이면 
+		if( msgBox.msg.type == 'alarm' ){
+			html = `${typeHTML( msgBox.msg )}`;
 		}
-		// 만약에 일반 메시지이면
+		// 만약에 메시지 타입이 알림이 아니면 [ 메시지 , 이모티콘 ]
 		// 2-2 만약에 내가 보냈으면. [ 보낸사람아이디와 로그인된사람의 아이디와 같으면 ]
 		else if( msgBox.frommid == loginMid ){
 				html = `<div class="rcont"> 
@@ -137,7 +128,7 @@ function getEmo(){
 // 7. 클릭한 이모티콘 서버로 보내기.
 function onEmoSend( i ){
 	// 1. msg 구성 
-	let msg = { type : 'emo' , content : i+""  };	//i+"" 하는 이유가 replace는 문자열만 가능
+	let msg = { type : 'emo' , content : i+""  }; // i+"" 하는 이유 가 replace는 문자열만 가능 
 		// type : msg[메시지] , emo[이모티콘] , img[사진]
 		// content : 내용물 
 		
@@ -150,7 +141,6 @@ function onEmoSend( i ){
 function typeHTML( msg ){
 	
 	let html = ``;
-	
 	// 1. 메시지 타입 일때는 <div> 반환  
 	if( msg.type == 'message'){
 		html += `<div class="content"> ${ msg.content } </div>`;
@@ -159,13 +149,11 @@ function typeHTML( msg ){
 	else if( msg.type == 'emo' ){
 		html += `<img src="/jspweb/img/imoji/emo${msg.content}.gif" />`;
 	}
-	// 3. 만약에 알림 타입 일때는 <div> 반환
-	else if(msg.type == 'alarm'){
-		html += `<div class="alarm"> ${msg.content} </div>`;
+	// 3. 만약에 알림 타입 일때는 <div> 반환 
+	else if( msg.type == 'alarm' ){
+		html += `<div class="alarm"> ${ msg.content } </div>`;
 	}
-	
 	return html;
-	
 }
 
 
@@ -376,4 +364,23 @@ function onmsg(event){
 				<--------------------------------
 				3. 서버가 '안녕' 메시지 보낸다.
 		
+*/
+
+/*
+	let msgBox = JSON.parse( e.data );	console.log( msgBox );
+		// JSON.parse( ) 		: 문자열타입의 JSON형식을 JSON타입으로 변환 
+		// JSON.stringify( ) 	: JSON타입 을 문자열 타입 (JSON형식 유지)으로 변환 
+		console.log( msgBox.msg ); // java,js console내 출력시 줄바꿈 \n 맞음.. html에서의 줄바꿈 <br>
+		
+		// 1. 특정 문자열 찾아서 1개 치환/바꾸기/교체 
+		let content = msgBox.msg.replace( '\n' , '<br>' );	// replace( '변경할문자열|정규표현식' , '새로운문자' );
+		console.log( content );
+		// 2. 특정 문자열 찾아서 찾은 문자열 모두 치환/바꾸기/교체 => java : .replaceAll();   js : 정규표현식 
+		content  = msgBox.msg.replace( /\n/g , '<br>');	// /g : 동일한 패턴의 모든 문자찾기[전체]
+		console.log( content );
+	
+	msgBox.msg	= JSON.parse( msgBox.msg ); // 치환하고 대입.
+		console.log( msgBox );
+		
+
 */
